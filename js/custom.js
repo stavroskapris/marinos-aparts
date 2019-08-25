24706babc6bd6bb925af6d4c571251436a3b9a1a
StavrosKapris <stavros_zero@hotmail.com>
2019-08-25 23:25:10 +0300

    added photoswipe gallery,separated lang file

--- js/custom.js
+++ js/custom.js
@@ -1,64 +1,72 @@
-//var to hold selected language
-let lang;
-// noinspection JSUnresolvedFunction
-$(function ($) {
-    lang = localStorage.getItem('lang') || 'en';
-    if (lang == null) {
-        lang = $('#langSelect').val();
-        console.log(lang + ' mesa');
-    } else {
-        $('#langSelect').val(lang);
-    }
-
-    $('.lang').each(function () {
-        $(this).text(langData['languages'][lang]['keys'][$(this).data('key')])
-    });
-    //initialize map
-    initializeMap();
-});
-
 /**
+ * Holds selected language
  *
  */
-// noinspection JSUnresolvedFunction
-$('a.js-langanchor').on('click', function () {
-    // location.reload();
-    // noinspection JSUnresolvedFunction
-    lang = $(this).data('lang');
-    localStorage.setItem('lang', lang);
-    console.log(lang);
-    // noinspection JSUnresolvedFunction
-    $('.lang').each(function () {
-        // noinspection JSUnresolvedFunction
-        $(this).text(langData['languages'][lang]['keys'][$(this).data('key')])
-    });
-
-});
-
-let langData = {
-    'languages': {
-        'en': {
-            'keys': {
-                'home': 'Home',
-                'kimon': 'Kimon Resort',
-                'irida': 'Irida Resort',
-                'contact': 'Contact',
-                'welcome': 'Welcome to Marinos-aparts Rooms in Sivota, where your satisfaction is our top priority.'
-            }
-        }
-        , 'gr': {
-            'keys': {
-                'home': 'Αρχική',
-                'kimon': 'Kimon Resort',
-                'irida': 'Irida Resort',
-                'contact': 'Επικοικωνία',
-                'welcome': 'Καλωσορίσατε στα ενοικιαζόμενα δωμάτια Marinos apartments, στα Σύβοτα, όπου η απόλυτη ικανοποίηση σας είναι προτεραιότητα μας.'
-            }
-        }
-    }
-};
+let lang;
 
 /**
+ * Holds current page
+ */
+let page;
+
+/**
+ *Language menu
+ *
+ * @type {string}
+ */
+let langMenuEn = '<a href="javascript:void(0)" data-lang="en" class="js-langanchor" id="anchoren">\n' +
+    '                                <li><img src="img/f1ky10.jpg.gif" height="13" width="18"></li>\n' +
+    '                            </a>\n' +
+    '                            <a href="javascript:void(0)" data-lang="gr" class="js-langanchor" id="anchorgr">\n' +
+    '                                <li><img src="img/2r7t05j.jpg" height="13" width="18"></li>\n' +
+    '                            </a>';
+/**
+ *Language menu
+ *
+ * @type {string}
+ */
+let langMenuGr = ' <a href="javascript:void(0)" data-lang="gr" class="js-langanchor" id="anchorgr">\n' +
+    '                                <li><img src="img/2r7t05j.jpg" height="13" width="18"></li>\n' +
+    '                            </a>\n' +
+    '                            <a href="javascript:void(0)" data-lang="en" class="js-langanchor" id="anchoren">\n' +
+    '                                <li><img src="img/f1ky10.jpg.gif" height="13" width="18"></li>\n' +
+    '                            </a>';
+/**
+ * On page load
+ */
+$(function ($) {
+    //get lang if exists
+    lang = localStorage.getItem('lang') || 'en';
+    //get current page
+    page = $('#whichPage').val();
+    //configure language menu
+    configureLangMenu(lang);
+    //translate current page
+    translate(page);
+    //initialize map if in contact page
+    page === 'contact' ? initializeMap() : '';
+    //$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+/**
+ *Language menu on click function
+ */
+$("#js-langmenu").on('click', '.js-langanchor', function () {
+    // noinspection JSUnresolvedFunction
+    //get selected lang
+    lang = $(this).data('lang');
+    //store selected lang
+    localStorage.setItem('lang', lang);
+    //configure lang menu
+    configureLangMenu(lang);
+    // noinspection JSUnresolvedFunction
+    //replace with appropriate lang items
+    translate(page);
+});
+
+
+/**
+ * Populate map div with osm map
  *
  */
 function initializeMap() {
@@ -81,4 +89,68 @@ function initializeMap() {
 // Place a marker on the same location.
     L.marker(target1).addTo(map);
     L.marker(target2).addTo(map);
+}
+
+/**
+ * Configure language dropdown
+ *
+ * @param lang
+ */
+function configureLangMenu(lang) {
+    let langMenu = $('#js-langmenu');
+    lang === 'en' ? langMenu.html(langMenuEn) : langMenu.html(langMenuGr);
+}
+
+/**
+ *
+ * @param page
+ */
+function translate(page) {
+    $('.lang').each(function () {
+        //navbar
+        $(this).text(langData['languages'][lang]['pages'][page]['navbar'][$(this).data('key')]);
+        //rest of the page
+        $(this).text(langData['languages'][lang]['pages'][page][$(this).data('key')])
+    });
+}
+
+
+/* global jQuery, PhotoSwipe, PhotoSwipeUI_Default, console */
+
+function galleryInit() {
+
+    // Init empty gallery array
+    var container = [];
+
+    // Loop over gallery items and push it to the array
+    $('#gallery').find('figure').each(function () {
+        var $link = $(this).find('a'),
+            item = {
+                src: $link.attr('href'),
+                w: $link.data('width'),
+                h: $link.data('height'),
+                title: $link.data('caption')
+            };
+        container.push(item);
+    });
+
+    // Define click event on gallery item
+    $('.gallery').on('click', 'a', function (event) {
+
+        // Prevent location change
+        event.preventDefault();
+
+        // Define object and gallery options
+        var $pswp = $('.pswp')[0],
+            options = {
+                index: $(this).parent('figure').index(),
+                bgOpacity: 0.85,
+                showHideOpacity: true
+            };
+
+        // Initialize PhotoSwipe
+        var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, container, options);
+        gallery.init();
+    });
+
 }
---
+++ js/lang.js
@@ -0,0 +1,90 @@
+let langData = {
+    'languages': {
+        'en': {
+            'pages': {
+                'home': {
+                    'navbar': {
+                        'home': 'Home',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Contact',
+                    },
+                    'welcome': 'Welcome to Marinos-aparts Rooms in Sivota, where your satisfaction is our top priority.' +
+                        ' Marinos-aparts, with 25 years of hospitality, consist of 2 separate accommodation, KIMON ' +
+                        'RESORT and IRIDA RESORT.',
+                    'kimonTitle': 'KIMON RESORT, completely renovated in 2013, is the oldest of our accommodations.',
+                    'iridaTitle': 'IRIDA RESORT, our newest accommodation, fully refurbished in 2013 and located in ' +
+                        'the center, 30 meters from the port.',
+                },
+                'kimon': {
+                    'navbar': {
+                        'home': 'Home',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Contact',
+                    },
+                },
+                'irida': {
+                    'navbar': {
+                        'home': 'Home',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Contact',
+                    },
+                },
+                'contact': {
+                    'navbar': {
+                        'home': 'Home',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Contact',
+                    },
+                },
+            },
+        }
+        , 'gr': {
+            'pages': {
+                'home': {
+                    'navbar': {
+                        'home': 'Αρχική',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Επικοικωνία',
+                    },
+                    'welcome': 'Καλωσορίσατε στα ενοικιαζόμενα δωμάτια Marinos apartments, στα Σύβοτα, όπου η απόλυτη ' +
+                        'ικανοποίηση σας είναι προτεραιότητα μας.' +
+                        ' Τα Marinos-aparts, με 25 χρόνια παρουσίας στον χώρο της φιλοξενίας, αποτελούνται από 2 ' +
+                        'ξεχωριστά καταλύματα, το KIMON RESORT και το IRIDA RESORT.',
+                    'kimonTitle': 'To KIMON RESORT, πλήρως ανακαινισμένο το 2013, είναι το παλαιότερο από τα καταλύματα μας.',
+                    'iridaTitle': 'Το IRIDA RESORT, το νεότερο κατάλυμα μας, πλήρως ανακαινισμένο το 2013 και αυτό, ' +
+                        'βρίσκεται στο κέντρο, 30 μέτρα απο το λιμάνι.',
+                },
+                'kimon': {
+                    'navbar': {
+                        'home': 'Αρχική',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Επικοικωνία',
+                    },
+                },
+                'irida': {
+                    'navbar': {
+                        'home': 'Αρχική',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Επικοικωνία',
+                    },
+                },
+                'contact': {
+                    'navbar': {
+                        'home': 'Αρχική',
+                        'kimon': 'Kimon Resort',
+                        'irida': 'Irida Resort',
+                        'contact': 'Επικοικωνία',
+                    },
+                },
+            },
+
+        }
+    }
+};
