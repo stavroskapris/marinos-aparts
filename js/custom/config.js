cc14da48f3951d1cbcce73c5329ebb71548e002d
stavrosKap <stavros.kapridakis@spitogatos.gr>
2019-09-18 17:17:40 +0300

    update git ignore, added a todo

--- js/custom/config.js
+++ js/custom/config.js
@@ -3,7 +3,8 @@
  *
  * @type {string}
  */
+//TODO replace endpoints with xxxxxxx before final commit
 App.apiEndPoints = {
-    'contact': 'https://8vgfxd8lde.execute-api.eu-west-1.amazonaws.com/dev/contact',
-    'recaptcha': 'https://8vgfxd8lde.execute-api.eu-west-1.amazonaws.com/dev/validaterecaptcha'
-}
+    contact: 'https://8vgfxd8lde.execute-api.eu-west-1.amazonaws.com/dev/contact',
+    recaptcha: 'https://8vgfxd8lde.execute-api.eu-west-1.amazonaws.com/dev/validaterecaptcha'
+};
