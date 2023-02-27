82866b075aca6830f4e3b3b648c1e4372f0d5caa
stavroskapris <stavroskapridakis@gmail.com>
2023-02-27 12:29:58 +0200

    added book now li in navbar, hidden for now, updated lang items for new li

--- js/custom/lang/navbar_lang.js
+++ js/custom/lang/navbar_lang.js
@@ -4,19 +4,19 @@
  * @type {{en: {irida: string, contact: string, location: string, kimon: string, home: string}, gr: {irida: string, contact: string, location: string, kimon: string, home: string}}}
  */
 App.navbar_lang = {
-    en:
-        {
-            home: 'Home',
-            kimon: 'Kimon Resort',
-            irida: 'Irida Resort',
-            location: 'Location',
-            contact: 'Contact',
-        },
-    gr: {
+    en: {
+        home: 'Home',
+        kimon: 'Kimon Resort',
+        irida: 'Irida Resort',
+        location: 'Location',
+        contact: 'Contact',
+        book: 'Book now',
+    }, gr: {
         home: 'Αρχική',
         kimon: 'Kimon Resort',
         irida: 'Irida Resort',
         location: 'Τοποθεσία',
         contact: 'Επικοινωνία',
+        book: 'Κρατήσεις',
     },
 };
